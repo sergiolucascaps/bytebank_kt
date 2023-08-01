8300c2a0cf8b40fcdee73790b96b9daf78ea0dec
Sergio Lucas <sergiolucascaps@hotmail.com>
2023-08-01 20:38:13 -0300

    Initial Structure of Kotlin project @sergiolucascaps

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    print("Bem vindo ao bytebank")
+
+    val titular = "Sergio Lucas da Silva"
+    val numeroConta = 1000
+    var saldo = 0.0
+
+    println("Titular: $titular")
+    println("Número da conta: $numeroConta")
+    println("Saldo: $saldo")
+}
