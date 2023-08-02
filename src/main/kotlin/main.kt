82b699435785d9b8373fcbcd2cdd677834a68768
Sergio Lucas <sergiolucascaps@hotmail.com>
2023-08-01 21:27:27 -0300

    Create Conta class @sergiolucascaps

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,4 +8,28 @@ fun main() {
     println("Titular: $titular")
     println("Número da conta: $numeroConta")
     println("Saldo: $saldo")
+
+    var conta = Conta();
+    conta.titular = "Titular O.O"
+    conta.numeroConta = 1000
+    conta.saldo = 100.0
+
+    println(conta.titular)
+    println(conta.numeroConta)
+    println(conta.saldo)
+
+//    testaSaldo(saldo);
+}
+
+class Conta {
+    var titular = ""
+    var numeroConta = 0
+    var saldo = 0.0
+}
+fun testaSaldo(saldo: Double) {
+    when {
+        saldo > 0.0 -> println("Conta positiva")
+        saldo == 0.0 -> println("Conta neutra")
+        saldo < 0.0 -> println("COnta negativa")
+    }
 }
